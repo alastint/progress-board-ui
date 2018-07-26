fad5e0e98db2edb593a37910b118f7909a312428
neitiris <veliantos@gmail.com>
2018-07-26 14:12:00 +0300

    created post functional of chat

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../../services/authservice';
 import {Router} from '@angular/router';
+import {ApiService} from '../../../services/api';
 
 @Component({
   selector: 'app-homepage',
@@ -10,6 +11,7 @@ import {Router} from '@angular/router';
 export class HomepageComponent implements OnInit {
   public chat: any = { message: '' };
   public chatMessages: any[] = [];
+  private authorId = 0;
   public studentsScore: any[] =  [
     { position: 1, email: 'some@email.com', score: 817, rank: 'Mentor', },
     { position: 2, email: 'some@email.com', score: 717, rank: 'Mentor', },
@@ -25,23 +27,47 @@ export class HomepageComponent implements OnInit {
 
   constructor(
     public authservice: AuthService,
-    public router: Router
+    public api: ApiService
   ) { }
 
   public ngOnInit() {
   }
+  public getUserData() {
+    // Retrieve data by key from local storage
+    const userString: string = localStorage.getItem('currentUser');
+    // Return user object if data in local storage exist, or empty object if no user data available
+    return typeof userString === 'string' ? JSON.parse(userString) : {};
+  }
+  public getMessageData() {
+    let messageString = '';
+    this.api.get(`/message/12`).subscribe(
+      (resp: any) => {
+        messageString = resp.userId;
+      }
+    );
+    console.log('messageString', messageString);
+    return messageString;
+  }
 
   public sendMessage(text: string) {
-    const author1: any = { name: 'me', id: 1 };
-    const author2: any = { name: 'Some idiot', id: 2 };
-    const id: number = this.chatMessages.length + 1;
+    const author1: any = { name: 'me', authorId: 1 };
+    const author2: any = { name: 'Some idiot', authorId: 2 };
+    const appUser: any = this.getUserData();
+    const messages: any = this.getMessageData();
+    console.log('appUser.id', appUser.id, 'messages', messages);
+    appUser.id === messages.userId ? this.authorId = 1 : this.authorId = 2;
     const message: any = {
       text,
-      id,
-      author: !(id % 2) ? author1.name : author2.name,
+      author: !(this.authorId % 2 && this.authorId !== 0) ? author1.name : author2.name,
       timestamp: new Date().toISOString()
     };
     this.chatMessages.push(message);
+    console.log('message.text', message.text);
+    this.api.post(`/message`, {text: message.text, userId: appUser.id}).subscribe(
+      (resp: any) => {
+        console.log('resp', resp);
+      }
+    );
     this.chat.message = '';
   }
   public quit() {
