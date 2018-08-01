ce1863e63adf36e864511189cab4fa809071bc62
neitiris <veliantos@gmail.com>
2018-08-01 13:43:18 +0300

    fixed chat naming and position of interlocutors

--- src/app/components/chat-component/chat-component.component.ts
+++ src/app/components/chat-component/chat-component.component.ts
@@ -11,7 +11,7 @@ export class ChatComponentComponent {
   public urlParams = '';
   public chat: any = { message: ''};
   public chatMessages: any[] = [];
-  private authorId = 0;
+  public author = '';
   @Input() options: any = { };
   @Output() doConfirm: any = new EventEmitter();
 
@@ -29,18 +29,18 @@ export class ChatComponentComponent {
    */
   public loadChat() {
     const appUser: any = this.messageService.getUserData();
+    console.log('appUser', appUser.fullname);
     this.urlParams = `?page=1&limit=10&order={"createdAt":-1}`;
     console.log(this.urlParams);
     this.messageService.getMessage(this.urlParams).subscribe(
       (responseLoad: any) => {
         console.log('responseLoad', responseLoad);
         for (let i = responseLoad.rows.length - 1; i >= 0; i--) {
-          this.authorId = appUser.id === responseLoad.rows[i].userId ? 1 : 2;
           if (!(responseLoad.rows[i].status == '[NEWS]')) {
-            console.log(i);
+            console.log('appUser.id === responseLoad.rows[i].userId', appUser.id === responseLoad.rows[i].userId);
             const historyMessage = {
+              author: (appUser.id === responseLoad.rows[i].userId) ? 'Me' : 'Interlocutor',
               text: responseLoad.rows[i].text,
-              align: (this.authorId % 2 || this.authorId === 0) ? 1 : 2,
               timestamp: responseLoad.rows[i].createdAt
             };
             this.chatMessages.push(historyMessage);
@@ -68,17 +68,14 @@ export class ChatComponentComponent {
    * @param {string} text
    */
   public sendMessage(text: string) {
-    const author1: any = { name: 'me', authorId: 1 };
-    const author2: any = { name: 'Some idiot', authorId: 2 };
     const appUser: any = this.messageService.getUserData();
     const message: any = {
       text: this.chat.message,
-      author: !(this.authorId % 2 && this.authorId !== 0) ? author1.name : author2.name,
+      author: appUser.fullname + ' ',
       timestamp: new Date().toISOString()
     };
     console.log('appUser.id',message.text, appUser.id);
     this.chatMessages.push(message);
-    // this.api.post(`/message`, {text: message.text, userId: appUser.id})
     this.messageService.postMessage(message.text, appUser.id).subscribe(
       (resp: any) => {
         console.log('resp', resp);
